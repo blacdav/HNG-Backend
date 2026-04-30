78463fa9bc7c69ed34c47577f7225b5f51e50284
blacdav <davidaniefoik@gmail.com>
2026-04-30 13:15:13 +0100

    feat: added rate limiting

---
+++ src/middlewares/rate-limit.middleware.js
@@ -0,0 +1,27 @@
+import { rateLimit } from 'express-rate-limit';
+
+export const AuthLimit = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    limit: 10,
+    standardHeaders: "draft-8",
+    legacyHeaders: true,
+    ipv6Subnet: 60,
+    keyGenerator: (req) => req.ip || "unknown",
+    message: {
+        status: "error",
+        message: "Too many requests, please try again later"
+    }
+});
+
+export const ApiLimit = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    limit: 60,
+    standardHeaders: "draft-8",
+    legacyHeaders: true,
+    ipv6Subnet: 60,
+    keyGenerator: (req) => req.ip || "unknown",
+    message: {
+        status: "error",
+        message: "Too many requests, please try again later"
+    }
+});
