bd33dd72492c352d569d68ae7151b8a9d04e7342
blacdav <davidaniefoik@gmail.com>
2026-05-01 09:19:28 +0100

    feat(update): added role permissions for admin, and updated the cors

--- src/profiles/routes/index.routes.js
+++ src/profiles/routes/index.routes.js
@@ -5,14 +5,15 @@ import { DeleteProfile } from "../controllers/delete.profile.js";
 import { FilterProfile } from "../controllers/filter.profile.js";
 import { SearchProfile } from "../controllers/search.controller.js";
 import { ExportProfile } from "../controllers/export.profile.js";
+import { AdminAuth } from "../../middlewares/admin.middleware.js";
 
 const router = Router();
 
-router.post('/', CreateProfile);
+router.post('/', AdminAuth, CreateProfile);
 router.get('/', FilterProfile);
 router.get('/search', SearchProfile);
 router.get('/export', ExportProfile);
 router.get('/:id', GetProfile);
-router.delete('/:id', DeleteProfile);
+router.delete('/:id', AdminAuth, DeleteProfile);
 
 export default router;
