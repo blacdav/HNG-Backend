04183a1fabd7a02307936843c1810e4679af0c16
blacdav <davidaniefoik@gmail.com>
2026-05-01 09:43:45 +0100

    feat(update): added role permissions for admin

---
+++ src/middlewares/admin.middleware.js
@@ -0,0 +1,23 @@
+import models from "../models/index.model.js";
+
+const { User } = models;
+
+export const AdminAuth = (req, res, next) => {
+    const { github_id, role } = req.user;
+
+    if (!role || role === "") {
+        return res.status(404).json({
+            status: "error",
+            message: "Session Compromized, role not passed"
+        })
+    }
+    
+    if (role !== "admin") {
+        return res.status(403).json({
+            status: "failed",
+            message: "This permission is reserved for only admin"
+        });
+    }
+
+    return next()
+}
